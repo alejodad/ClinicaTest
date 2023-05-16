57090adcddd20133e1440a76723712d5e518b791
alejodad <31315475+alejodad@users.noreply.github.com>
2023-05-16 13:19:22 -0500

    Update citaStepDefinitions.java

--- src/test/java/stepdefinitions/citaStepDefinitions.java
+++ src/test/java/stepdefinitions/citaStepDefinitions.java
@@ -7,7 +7,12 @@ import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import task.Login;
 import task.OpenTheBrowser;
 import task.SearchDate;
@@ -16,9 +21,10 @@ import userinterfaces.FamisanarHomeLogin;
 import userinterfaces.StreamProjectHome;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class citaStepDefinitions {
-    private FamisanarHomeLogin streamProjectHome;
+    private CafamHomeLogin streamProjectHome;
     @Managed(driver = "chrome")
     private WebDriver hisBrowser;
     ArrayList al = new ArrayList();
@@ -27,6 +33,8 @@ public class citaStepDefinitions {
     public void setUp() {
         alejo.can(BrowseTheWeb.with(hisBrowser));
         hisBrowser.manage().window().maximize();
+        hisBrowser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
     }
 
     @Given("Usuario esta en pagina principal")
@@ -35,16 +43,16 @@ public class citaStepDefinitions {
         alejo.wasAbleTo(
                 Login.theApp()
         );
-        //alejo.can(BrowseTheWeb.with(hisBrowser));
-        //alejo.wasAbleTo(
-          //      OpenTheBrowser.on(streamProjectHome));
+        new WebDriverWait(hisBrowser, 10).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]")));
+        new WebDriverWait(hisBrowser, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.rc-anchor-center-container"))).click();
+        hisBrowser.switchTo().defaultContent();
 
     }
 
     @When("Busca cita mas cercana")
     public void busca_cita_mas_cercana() {
         // Write code here that turns the phrase above into concrete actions
-        hisBrowser.switchTo().frame("ifAuto");
+        //hisBrowser.switchTo().frame("ifAuto");
         alejo.attemptsTo(
                 SearchDate.theApp()
         );
